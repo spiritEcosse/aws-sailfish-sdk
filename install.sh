5f30a49f5bd3ed2979f6900e0aac10d16b379c15
igor <shevchenkcoigor@gmail.com>
2022-01-23 18:18:55 +0200

    add log

--- install.sh
+++ install.sh
@@ -23,7 +23,8 @@ function install_docker() {
 	# install docker
 	# https://docs.docker.com/engine/install/ubuntu/
 
-	if [ -x "$(docker -v)" ]; then
+	if [[ -x "$(docker -v)" ]]; then
+		log_app_msg "pre docker already installed."
 		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 		echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 		sudo apt-get update -y && \
