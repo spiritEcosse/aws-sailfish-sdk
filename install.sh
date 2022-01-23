db380390d3da3dcc571087bd0d195e6a739f2aa5
igor <shevchenkcoigor@gmail.com>
2022-01-23 18:42:35 +0200

    fix libGL error: failed to load driver: swrast and libGL error: No matching fbConfigs or visuals found

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ RED='\033[0;31m'
 BLUE='\033[1;36m'
 SDK_VERSION='3.7.4'
 SDK_FILE_NAME="SailfishSDK-${SDK_VERSION}-linux64-offline.run"
+export LIBGL_ALWAYS_INDIRECT=1
 
 function log_app_msg() {
 	echo -ne "[${BLUE}INFO${NC}] $@\n"
@@ -16,7 +17,7 @@ function log_failure_msg() {
 
 function install_deps() {
 	sudo apt update -y && \
-	sudo apt install -y libxcb1 libx11-xcb1 libxcb1 libxcb-glx0 libfontconfig1 libx11-data libx11-xcb1 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxcb-sync1 libxcb-xfixes0 libxcb-xinerama0 libxcb-xkb1 libsm6 libxkbcommon-x11-0 libwayland-egl1 libegl-dev libxcomposite1 libwayland-cursor0 libharfbuzz-dev libxi-dev libtinfo5 ca-certificates curl gnupg lsb-release
+	sudo apt install -y libxcb1 libx11-xcb1 libxcb1 libxcb-glx0 libfontconfig1 libx11-data libx11-xcb1 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxcb-sync1 libxcb-xfixes0 libxcb-xinerama0 libxcb-xkb1 libsm6 libxkbcommon-x11-0 libwayland-egl1 libegl-dev libxcomposite1 libwayland-cursor0 libharfbuzz-dev libxi-dev libtinfo5 ca-certificates curl gnupg lsb-release mesa-utils libgl1-mesa-glx
 }
 
 function install_docker() {
@@ -24,7 +25,6 @@ function install_docker() {
 	# https://docs.docker.com/engine/install/ubuntu/
 
 	if [[ -x "$(docker -v)" ]]; then
-		log_app_msg "pre docker already installed."
 		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 		echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 		sudo apt-get update -y && \
