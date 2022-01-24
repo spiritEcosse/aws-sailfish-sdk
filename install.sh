05345f84092b42ffbbccfb01ac5506db9e1a4343
igor <shevchenkcoigor@gmail.com>
2022-01-24 10:32:48 +0200

    add install_virtualbox

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ BLUE='\033[1;36m'
 SDK_VERSION='3.7.4'
 SDK_FILE_NAME="SailfishSDK-${SDK_VERSION}-linux64-offline.run"
 export LIBGL_ALWAYS_INDIRECT=1
+export GITHUB_TOKEN="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"
+export GITHUB_USER="spiritEcosse"
 
 function log_app_msg() {
 	echo -ne "[${BLUE}INFO${NC}] $@\n"
@@ -17,7 +19,11 @@ function log_failure_msg() {
 
 function install_deps() {
 	sudo apt update -y && \
-	sudo apt install -y libxcb1 libx11-xcb1 libxcb1 libxcb-glx0 libfontconfig1 libx11-data libx11-xcb1 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxcb-sync1 libxcb-xfixes0 libxcb-xinerama0 libxcb-xkb1 libsm6 libxkbcommon-x11-0 libwayland-egl1 libegl-dev libxcomposite1 libwayland-cursor0 libharfbuzz-dev libxi-dev libtinfo5 ca-certificates curl gnupg lsb-release mesa-utils libgl1-mesa-glx
+	sudo apt install -y libxcb1 libx11-xcb1 libxcb1 libxcb-glx0 libfontconfig1 libx11-data libx11-xcb1 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxcb-sync1 libxcb-xfixes0 libxcb-xinerama0 libxcb-xkb1 libsm6 libxkbcommon-x11-0 libwayland-egl1 libegl-dev libxcomposite1 libwayland-cursor0 libharfbuzz-dev libxi-dev libtinfo5 ca-certificates curl gnupg lsb-release mesa-utils libgl1-mesa-glx micro
+}
+
+function install_virtualbox() {
+	sudo apt-get install -y virtualbox
 }
 
 function install_docker() {
@@ -63,13 +69,24 @@ function theme_qtcreator() {
 
 function bible_project() {
 	if [ ! -d "bible" ]; then
-		git clone https://github.com/spiritEcosse/bible.git
+		git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/bible.git
 		log_app_msg "Project bible has downloaded successfully."
 	fi
 }
 
+function grt_aliases() {
+	git config --global alias.co checkout                                                                                                                                        ─╯
+	git config --global alias.ci commit
+	git config --global alias.st status
+	git config --global alias.br branch
+	git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
+	git config --global alias.type 'cat-file -t'
+	git config --global alias.dump 'cat-file -p'
+}
+
 install_deps && \
-install_docker && \
+install_virtualbox && \
+#install_docker && \
 download_sailfish_sdk && \
 theme_qtcreator && \
 bible_project && \
