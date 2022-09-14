8dc4d97cfcd0289a753d9f85b22ea9a6c82af098
spiritEcosse <shevchenkcoigor@gmail.com>
2022-09-14 18:34:42 +0200

    add functions from main proj

--- install.sh
+++ install.sh
@@ -30,6 +30,170 @@ while (( ${#} > 0 )); do
   shift
 done
 
+if [[ -z ${ARCH+x} ]]; then
+  ARCH=$(uname -m)
+fi
+
+PLATFORM=$(get_name_platform)
+BUILD_FOLDER=$(get_name_platform)_${ARCH}
+BUILD_FOLDER_FULL_PATH=/home/mersdk/"${BUILD_FOLDER}"/
+FILE=$(get_name_platform)_${ARCH}.tar.gz
+BACKUP_FILE_PATH=~/backups/"${FILE}"
+
+get_name_platform() {
+  awk -F= '$1=="ID" { print $2 ;}' /etc/os-release
+}
+
+aws_get_host() {
+  EC2_INSTANCE_HOST=$(aws ec2 describe-instances --instance-ids "${EC2_INSTANCE}" --query "Reservations[*].Instances[*].[PublicIpAddress]" --output text)
+  while [[ "$EC2_INSTANCE_HOST" == "None" ]]
+  do
+    sleep 5
+    aws_get_host
+  done
+}
+
+aws_wait_status_running() {
+  EC2_INSTANCE_STATUS=$(aws_get_instance_status)
+  while [[ "$EC2_INSTANCE_STATUS" != "running" ]]
+  do
+    sleep 5
+    aws_wait_status_running
+  done
+}
+
+aws_stop() {
+  aws ec2 stop-instances --instance-ids "${EC2_INSTANCE}"
+}
+
+aws_get_instance_status() {
+  aws ec2 describe-instance-status --instance-ids "${EC2_INSTANCE}" --query "InstanceStatuses[*].InstanceState.Name" --output text
+}
+
+install_aws() {
+  if ! which aws; then
+    if "$PLATFORM" = "ubuntu"; then
+      install_for_ubuntu pip
+    elif "$PLATFORM" = "sailfishos"; then
+      sudo zypper -n install python3-pip
+    fi
+
+    sudo pip install awscli
+    aws --version
+    mkdir ~/.aws/
+    echo "[default]
+    region = ${AWS_REGION}" > ~/.aws/config
+  fi
+}
+
+set_up_instance_aws_host_to_known_hosts () {
+  if ! grep "$1" ~/.ssh/known_hosts; then
+      SSH_KEYSCAN=$(ssh-keyscan -T 180 -H "$1")
+      printf "#start %s\n%s\n#end %s\n" "$1" "$SSH_KEYSCAN" "$1" >> ~/.ssh/known_hosts
+      ssh -i "${SSH_ID_RSA}" "${EC2_INSTANCE_USER}"@"$1" "sudo shutdown +60"
+
+      if [[ -d ".idea" ]]; then
+        sed -i '' -e "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/$1/g" .idea/webServers.xml .idea/sshConfigs.xml
+      fi
+  fi
+}
+
+aws_start() {
+  if [[ $(aws_get_instance_status) != "running" ]]; then
+    aws ec2 start-instances --instance-ids "${EC2_INSTANCE}"
+    aws_wait_status_running
+    sleep 60
+  fi
+}
+
+sfdk_deploy_to_device() {
+  source ~/.zshrc
+  eval sfdk tools list
+  eval sfdk device list
+  eval sfdk config device=\'Xperia 10 - Dual SIM \(ARM\)\'
+  eval sfdk config target=SailfishOS-4.4.0.58-"$ARCH"
+  cd build-bible-SailfishOS_4_4_0_58_"$ARCH"_in_sailfish_sdk_build_engine_ubuntu-Debug
+  eval sfdk build ../bible
+  eval sfdk deploy --sdk
+  eval sfdk device exec /usr/bin/bible &
+  eval sfdk device exec journalctl -f /usr/bin/bible
+}
+
+sailfish_run_tests_on_aws() {
+  source ~/.zshrc
+  eval sfdk tools list
+  eval sfdk config target=SailfishOS-4.4.0.58-"$ARCH"
+  cd build-bible-SailfishOS_4_4_0_58_"$ARCH"_in_sailfish_sdk_build_engine_ubuntu-Debug
+  eval sfdk build ../bible -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_TESTING=ON -DCODE_COVERAGE=ON
+  eval sfdk build-shell ctest --output-on-failure
+}
+
+sfdk_run_app_on_device() {
+  # devel-su usermod -a -G systemd-journal nemo
+  source ~/.zshrc
+  eval sfdk device list
+  eval sfdk config device=\'Xperia 10 - Dual SIM \(ARM\)\'
+  eval sfdk device exec /usr/bin/bible &
+  eval sfdk device exec journalctl -f /usr/bin/bible
+}
+
+prepare_aws_instance() {
+  install_aws
+  aws_start
+  aws_get_host
+  set_up_instance_aws_host_to_known_hosts "${EC2_INSTANCE_HOST}"
+}
+
+upload_backup() {
+  cd /home/mersdk/
+  tar -zcf "${FILE}" "${BUILD_FOLDER}"
+  rsync -e "ssh -i ${SSH_ID_RSA}" -av --partial --inplace --append --progress "${FILE}" "${EC2_INSTANCE_USER}"@"${EC2_INSTANCE_HOST}":~/backups/
+  aws_stop
+}
+
+mb2_cmake_build() {
+  cd "${BUILD_FOLDER_FULL_PATH}"
+  mb2 build-init
+  mb2 build-requires
+  mb2 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_TESTING=ON -DCODE_COVERAGE=ON
+  mb2 cmake --build ../bible
+}
+
+mb2_run_tests() {
+  cd "${BUILD_FOLDER_FULL_PATH}"
+  mb2 build-shell ctest --output-on-failure
+}
+
+mb2_run_ccov_all_capture() {
+  cd "${BUILD_FOLDER_FULL_PATH}"
+  mkdir ccov
+  mb2 build-shell make ccov-all-capture
+}
+
+codecov_push_results() {
+  cd "${BUILD_FOLDER_FULL_PATH}"
+  curl -Os https://uploader.codecov.io/latest/linux/codecov
+  chmod +x codecov
+  ./codecov -t "${CODECOV_TOKEN}" -f ccov/all-merged.info
+}
+
+rsync_share_to_bible() {
+  mkdir -p /home/mersdk/bible/
+  cd /home/mersdk/bible/
+  sudo rsync -rv --checksum --ignore-times --info=progress2 --stats --human-readable --exclude '.git/modules' /share/ .
+  sudo chown -R mersdk:mersdk .
+}
+
+code_coverage() {
+  rsync_share_to_bible &
+  mb2_cmake_build
+  upload_backup &
+  mb2_run_tests
+  mb2_run_ccov_all_capture
+  codecov_push_results
+  wait
+}
+
 system_prepare_ubuntu() {
     sudo apt-get update -y
     sudo apt-get upgrade -y
@@ -38,16 +202,24 @@ system_prepare_ubuntu() {
 }
 
 install_for_ubuntu() {
-    for lib in "$@"
-    do
-        if [[ ! $(dpkg -s ${lib}) ]]
-        then
-            echo "============================================= install ${lib} ===================================================="
-            sudo apt-get install -y ${lib}
-        fi
+  programs=()
 
-        programs+=("${lib}: `dpkg -s ${lib} | grep Status && dpkg -s ${lib} | grep Version || echo 'Not installed'`\n")
-    done
+  for lib in "$@"
+  do
+      if [[ ! $(dpkg -s ${lib}) ]]
+      then
+          echo "============================================= install ${lib} ===================================================="
+          sudo apt-get install -y ${lib}
+      fi
+
+      programs+=("${lib}: `dpkg -s ${lib} | grep Status && dpkg -s ${lib} | grep Version || echo 'Not installed'`\n")
+  done
+
+  if [[ "${programs}" ]]
+  then
+    log_app_msg "Installed programs: "
+    echo -e "${programs[@]}"
+  fi
 }
 
 log_app_msg() {
@@ -66,13 +238,7 @@ set_tz() {
 
 install_deps() {
 	system_prepare_ubuntu
-	programs=()
 	install_for_ubuntu sudo systemd libxcb1 libx11-xcb1 libxcb1 libxcb-glx0 libfontconfig1 libx11-data libx11-xcb1 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxcb-sync1 libxcb-xfixes0 libxcb-xinerama0 libxcb-xkb1 libsm6 libxkbcommon-x11-0 libwayland-egl1 libegl-dev libxcomposite1 libwayland-cursor0 libharfbuzz-dev libxi-dev libtinfo5 ca-certificates curl gnupg lsb-release mesa-utils libgl1-mesa-glx micro lsb-release sudo zsh git
-	if [[ $programs ]]
-    then
-        log_app_msg "Installed programs: "
-        echo -e "${programs[@]}"
-    fi
 }
 
 install_virtualbox() {
@@ -153,15 +319,21 @@ exec ~/SailfishOS/bin/sfdk "$@"' > ~/bin/sfdk
   chmod +x ~/bin/sfdk
 }
 
-sfdk_tools_lis() {
+sfdk_tools_list() {
   sfdk tools list
 }
 
 set_ssh() {
-  mkdir -p "$HOME"/.ssh
-  chmod 0700 "$HOME"/.ssh
-  if [ ! -f "$SSH_ID_RSA" ]; then
-    ssh-keygen -t rsa -q -f "$SSH_ID_RSA" -N ""
+  if [ ! -f "${SSH_ID_RSA}" ]; then
+    mkdir -p "$HOME"/.ssh
+    chmod 0700 "$HOME"/.ssh
+    chmod 600 "${SSH_ID_RSA}"
+
+    if [[ -z ${IDENTITY_FILE+x} ]]; then
+      ssh-keygen -t rsa -q -f "${SSH_ID_RSA}" -N ""
+    else
+      echo "${IDENTITY_FILE}" > "${SSH_ID_RSA}"
+    fi
   fi
 }
 
@@ -174,23 +346,74 @@ ssh_copy_id() {
 set_up_instance_host_to_known_hosts () {
   if ! grep "$1" ~/.ssh/known_hosts; then
     SSH_KEYSCAN=$(ssh-keyscan -T 180 -H "$1")
-    printf "#start %s\n%s\n#end %s" "$1" "$SSH_KEYSCAN" "$1" >> ~/.ssh/known_hosts
+    printf "#start %s\n%s\n#end %s\n" "$1" "$SSH_KEYSCAN" "$1" >> ~/.ssh/known_hosts
+  fi
+}
+
+create_user_mersdk() {
+  sudo adduser mersdk
+}
+
+download_backup() {
+  echo "${EC2_INSTANCE_USER}"
+  echo "${BACKUP_FILE_PATH}"
+  echo "${SSH_ID_RSA}"
+  CHUNKS=$(python3 -c "print(10 * 1024 * 1024)")
+
+  system_prepare_ubuntu
+  install_for_ubuntu pip openssh-client openssl curl
+  set_ssh
+#  touch ~/.ssh/known_hosts
+  prepare_aws_instance
+  mkdir -p ~/backups/
+  cd ~/backups/
+
+  SIZE_BACKUP_FILE=$(ssh -i "${SSH_ID_RSA}" "${EC2_INSTANCE_USER}"@"${EC2_INSTANCE_HOST}" "stat -c%s ${BACKUP_FILE_PATH}")
+
+  if [[ "${SIZE_BACKUP_FILE}" ]]; then
+    HASH_ORIGINAL=$(ssh -i "${SSH_ID_RSA}" "${EC2_INSTANCE_USER}"@"${EC2_INSTANCE_HOST}" "openssl sha256 ${BACKUP_FILE_PATH} | awk -F'= ' '{print $2}'")
+    SEC=$SECONDS
+
+    count=0
+    start=0
+    for i in `seq 0 ${CHUNKS} ${SIZE_BACKUP_FILE}`; do
+    	end=$(python3 -c "start = int(${start})
+end = int(start + ${CHUNKS} - 1)
+size = int(${SIZE_BACKUP_FILE})
+print(size if end > size else end)")
+      curl -r "${start}"-"${end}" -u "${EC2_INSTANCE_USER}": scp://"${EC2_INSTANCE_HOST}"/"${BACKUP_FILE_PATH}" -o "${BACKUP_FILE_PATH}_${count}" &
+    	count=$(( "${count}" + 1 ))
+    	start=$(python3 -c "print(int(${start}) + int(${CHUNKS}))")
+    done
+    wait
+    echo "after downloads : $(( SECONDS - SEC ))"
+
+    for i in $(ls "${BACKUP_FILE_PATH}_*" | sort -V); do
+    	cat "${i}" >> "${BACKUP_FILE_PATH}";
+    done
+
+    HASH=$(openssl sha256 "${BACKUP_FILE_PATH}" | awk -F'= ' '{print $2}')
+    [ "$HASH_ORIGINAL" = "$HASH" ]
+
+    tar -xf "${FILE}"
+  else
+    mkdir -p "${BUILD_FOLDER_FULL_PATH}"
   fi
 }
 
 main() {
-  create_spec_dirs
-  set_envs
+  create_spec_dirs &
+  set_envs &
+  sfdk_put_to_bin &
   install_deps
   install_docker
-  install_ohmyzsh
-  set_tz
-  set_zsh_by_default
-  set_ssh
+  install_ohmyzsh &
+  set_tz &
+  set_zsh_by_default &
+  set_ssh &
   sfdk_download
   sfdk_install
-  sfdk_put_to_bin
-  sfdk_tools_lis
+  sfdk_tools_list
 }
 
 $func
