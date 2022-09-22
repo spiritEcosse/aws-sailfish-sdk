facdedb39eaa6ac300e8e62b0c690343795636e3
spiritEcosse <shevchenkcoigor@gmail.com>
2022-09-22 09:27:22 +0200

    fix: build from cache

--- install.sh
+++ install.sh
@@ -191,10 +191,10 @@ upload_backup() {
 
 mb2_cmake_build() {
   cd "${BUILD_FOLDER}"
-#  rsync -rv --checksum --ignore-times --info=progress2 --stats --human-readable --exclude '.git/modules' "${HOME}"/bible/ "${BUILD_FOLDER}"
+  rsync -rv --checksum --ignore-times --info=progress2 --stats --human-readable --exclude '.git/modules' "${HOME}"/bible/ "${BUILD_FOLDER}"
   mb2 build-init
   mb2 build-requires
-  mb2 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_TESTING=ON -DCODE_COVERAGE=ON ../bible
+  mb2 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_TESTING=ON -DCODE_COVERAGE=ON
   mb2 cmake --build .
 }
 
