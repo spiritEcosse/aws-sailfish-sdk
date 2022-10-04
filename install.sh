cf164648f278ce6e0c41d164f0af0712757e3409
spiritEcosse <shevchenkcoigor@gmail.com>
2022-10-04 14:31:12 +0200

    fix: tar: sailfishos_aarch64: file changed as we read it

--- install.sh
+++ install.sh
@@ -310,7 +310,7 @@ code_coverage() {
   download_backup
   rsync_share_to_build
   mb2_cmake_build
-  upload_backup &
+  upload_backup
   mb2_run_tests
   mb2_run_ccov_all_capture
   codecov_push_results
