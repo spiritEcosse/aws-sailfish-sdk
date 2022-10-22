45a64c4fb5dcc64f008de38ae2a8e366bec74ba7
spiritEcosse <shevchenkcoigor@gmail.com>
2022-10-22 17:45:02 +0200

    replace xargs on awk

--- install.sh
+++ install.sh
@@ -150,7 +150,7 @@ set_up_instance_aws_host_to_known_hosts () {
     printf "#start %s\n%s\n#end %s\n" "$1" "$SSH_KEYSCAN" "$1" >> ~/.ssh/known_hosts
 
     get_ec2_instance_identify_file
-    echo "${IDENTITY_FILE}" | xargs -n 1 > "${TEMP_SSH_ID_RSA}"
+    echo "${IDENTITY_FILE}" | awk '{gsub("\\\\n","\n")};1' > "${TEMP_SSH_ID_RSA}"
     cat "${TEMP_SSH_ID_RSA}"
     chmod 600 "${TEMP_SSH_ID_RSA}"
     cat "${SSH_ID_RSA_PUB}" | ssh -o StrictHostKeyChecking=no -i "${TEMP_SSH_ID_RSA}" "${EC2_INSTANCE_USER}@$1" 'cat >> ~/.ssh/authorized_keys'
