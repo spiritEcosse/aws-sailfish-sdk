a59b017ca0ffef7d50f95ca2bc9012f943258fa6
spiritEcosse <shevchenkcoigor@gmail.com>
2022-10-27 09:37:19 +0200

    add rsync_from_host_to_sever

--- install.sh
+++ install.sh
@@ -205,7 +205,7 @@ aws_start() {
 }
 
 rsync_from_host_to_sever() {
-  rsync ${RSYNC_PARAMS_UPLOAD_SOURCE_CODE} ~/projects/bible/bible/ "${EC2_INSTANCE_USER}@${EC2_INSTANCE_HOST}":~/bible
+  rsync ${RSYNC_PARAMS_UPLOAD_SOURCE_CODE} ~/projects/bible/bible/ "${EC2_INSTANCE_USER}@${EC2_INSTANCE_HOST}":"${BUILD_FOLDER}"
 }
 
 prepare_aws_instance() {
