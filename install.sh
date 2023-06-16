8357c7cf6fdbb09d7c58c41a62f3dd57a21b059a
spiritEcosse <shevchenkcoigor@gmail.com>
2023-06-16 15:02:50 +0200

    add password for device from docker container

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ install_bash() {
   BASH_VERSION=5.2
   NAME_BASH=bash-${BASH_VERSION}
 
-  set -uox pipefail
+  set -ux pipefail
   exe=`exec 2>/dev/null; readlink "/proc/$$/exe"`
   set -euox pipefail
   case "$exe" in
