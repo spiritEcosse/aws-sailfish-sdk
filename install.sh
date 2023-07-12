fb69522202d230a06d7de15df8d25a8f8d416426
spiritEcosse <shevchenkcoigor@gmail.com>
2023-07-12 10:41:00 +0200

    comment download_backup_from_aws due to configure project in src folder

--- install.sh
+++ install.sh
@@ -560,7 +560,8 @@ mb2_set_target() {
 
 code_coverage() {
   mkdir -p "${SRC}"
-  download_backup_from_aws
+  mkdir -p "${BUILD_FOLDER}"
+#  download_backup_from_aws
   rsync_share_to_src
   rsync_share_to_build
   mb2_cmake_build
