7f050e5c14480bef21936451dd97b069b8e832b1
spiritEcosse <shevchenkcoigor@gmail.com>
2023-08-03 08:32:28 +0200

    fix : remove chown_current_user for mb2_deploy_to_device and mb2_build

--- install.sh
+++ install.sh
@@ -485,7 +485,7 @@ download_backup_src_from_aws() {
 
 mb2_deploy_to_device() {
   cd "${SRC}"
-  chown_current_user
+#  chown_current_user
   install_aws
   mb2_build
   cd "${BUILD_FOLDER}/RPMS"
@@ -499,7 +499,7 @@ mb2_deploy_to_device() {
 
 mb2_build() {
   cd "${BUILD_FOLDER}"
-  chown_current_user
+#  chown_current_user
   mb2_set_target
   mb2 build "${SRC}"
 }
