b29a31650bd2f7c59ca3b91e1f05ab9a54890565
spiritEcosse <shevchenkcoigor@gmail.com>
2023-09-25 12:17:56 +0200

    fix: IndexError: list index out of range

--- get_notes.py
+++ get_notes.py
@@ -3,6 +3,11 @@ from jira import JIRA
 from os import environ as env
 from collections import defaultdict
 
+env['JIRA_USER'] = "shevchenkcoigor@gmail.com"
+env['JIRA_TOKEN'] = "ATATT3xFfGF08poOg-ZvIu7C3p-wr_j7kKZw4EFs__Jz5TSuWbWS4fZUfJJhAKWgwA9wzzznR2E2TFpHGlB2jwd7Ph440N8-94aoRY6Hwza9zZKYOLMW91qHvvPeffiVBSexom4hM1b26lOM3Bi29tAKzGSp7chhV319kqI8espkwu5wk3rr1OI=4565A944"
+env['JIRA_HOST'] = "https://cross-bible.atlassian.net"
+env['VERSION'] = "v1.0.0"
+
 
 def get_notes_from_jira():
     jira = JIRA(env['JIRA_HOST'], basic_auth=(env['JIRA_USER'], env['JIRA_TOKEN']))
@@ -12,7 +17,7 @@ def get_notes_from_jira():
         session.auth = (env['JIRA_USER'], env['JIRA_TOKEN'])
         d = defaultdict(list)
 
-        for issue in issues:
+        for issue in issues[:5]:
             d[issue.fields.issuetype].append(issue)
 
         notes_list = []
@@ -26,9 +31,9 @@ def get_notes_from_jira():
                     f"{env['JIRA_HOST']}/rest/dev-status/1.0/issue/details?issueId={int(issue.id)}"
                     f"&applicationType=github&dataType=pullrequest")
                 j = res.json()
-                pull_request = j['detail'][0]['pullRequests'][0]['url'] if j['detail'] else ""
-                last_commit = j['detail'][0]['branches'][0]['lastCommit']['url'] if j['detail'] else ""
-                notes_list.append(f"* [{issue.key}]({issue.permalink()}); {issue.fields.summary}; @spiritEcosse in {pull_request}; commit: {last_commit}")
+                pull_requests = j['detail'][0]['pullRequests']
+                pull_request = pull_requests[0]['url'] if pull_requests else ""
+                notes_list.append(f"* [{issue.key}]({issue.permalink()}); {issue.fields.summary}; @spiritEcosse in {pull_request}")
             notes_list.append("</details>")
         notes_list.append("\n## Other")
         notes_list.append(
