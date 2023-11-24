c01d731c458e4e90b7d999a87be0745712b88b02
spiritEcosse <shevchenkcoigor@gmail.com>
2023-11-24 20:34:05 +0100

    replace PORT on FOXY_HTTP_PORT

--- install.sh
+++ install.sh
@@ -955,7 +955,7 @@ aws_run_commands_simple() {
     export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
     export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
     export AWS_REGION=${AWS_REGION}
-    export PORT=${PORT}
+    export FOXY_HTTP_PORT=${FOXY_HTTP_PORT}
     curl https://spiritecosse.github.io/aws-sailfish-sdk/install.sh | bash -s -- --func=\"$1\"
   "
 }
