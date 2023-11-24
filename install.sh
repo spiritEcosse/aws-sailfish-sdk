bb43439a1d9a8d9d6b26d5b7e43acc19bfa43cca
spiritEcosse <shevchenkcoigor@gmail.com>
2023-11-24 17:29:51 +0100

    fix : "/usr/lib/x86_64-linux-gnu/libdrogon.a": Permission denied.

--- install.sh
+++ install.sh
@@ -509,7 +509,7 @@ cmake_build() {
     fi
     cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_TESTING=ON -DCODE_COVERAGE=ON -S "${SRC}" -B "${BUILD_FOLDER}" -DCMAKE_MODULE_LINKER_FLAGS_INIT=-L"${llvm_path_root}"lib/ -DCMAKE_SHARED_LINKER_FLAGS_INIT=-L"${llvm_path_root}"lib/ -DCMAKE_EXE_LINKER_FLAGS_INIT=-L"${llvm_path_root}"lib/ -DCMAKE_EXE_LINKER_FLAGS=-L"${llvm_path_root}"lib/ -DCMAKE_MODULE_LINKER_FLAGS=-L"${llvm_path_root}"lib/ -DCMAKE_SHARED_LINKER_FLAGS=-L"${llvm_path_root}"lib/
     cmake --build . -j "$((2 * $(getconf _NPROCESSORS_ONLN)))"
-    make install
+    sudo make install
 }
 
 get_last_modified_file() {
