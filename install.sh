1226a4cad4945cee5b75f402f043c39d6c8e39c2
spiritEcosse <shevchenkcoigor@gmail.com>
2023-11-24 20:19:51 +0100

    add variable PORT

--- install.sh
+++ install.sh
@@ -949,6 +949,7 @@ aws_run_commands_simple() {
     export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
     export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
     export AWS_REGION=${AWS_REGION}
+    export PORT=${PORT}
     curl https://spiritecosse.github.io/aws-sailfish-sdk/install.sh | bash -s -- --func=\"$1\"
   "
 }
