db836237781b53fdda3ca1adeb479007c0c52676
spiritEcosse <shevchenkcoigor@gmail.com>
2023-12-23 23:29:49 +0100

    Update app's port to 80 in install.sh

--- install.sh
+++ install.sh
@@ -541,7 +541,7 @@ def serve_file():
     return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=80)
 EOF"
     python3 foxy_server.py
 }
