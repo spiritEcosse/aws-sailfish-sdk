f0931242f81982a4413558cb5cee08f7a6a7861b
spiritEcosse <shevchenkcoigor@gmail.com>
2024-05-03 12:19:35 +0200

    Enable system upgrade and dist-upgrade in Ubuntu setup

--- install.sh
+++ install.sh
@@ -386,8 +386,8 @@ system_prepare_ubuntu() {
     fi
 
     sudo apt update -y
-#    sudo apt upgrade -y
-#    sudo apt dist-upgrade -y
+    sudo apt upgrade -y
+    sudo apt dist-upgrade -y
     # sudo dpkg --configure -a
 }
 
