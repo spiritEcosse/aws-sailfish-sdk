73985cee37f04f27e0844a488ac651f4a213aa3c
spiritEcosse <shevchenkcoigor@gmail.com>
2024-05-03 11:48:37 +0200

    Add supervisorctl function for managing processes

--- install.sh
+++ install.sh
@@ -591,6 +591,12 @@ environment=CONFIG_APP_PATH=${BUILD_FOLDER}/config.json,FOXY_HTTP_PORT=80,ENV=be
 EOF"
 }
 
+supervisorctl() {
+    sudo supervisorctl reread
+    sudo supervisorctl update
+    sudo supervisorctl restart foxy_server
+    sudo supervisorctl status
+}
 
 cmake_build() {
     system_prepare_ubuntu
@@ -607,10 +613,6 @@ cmake_build() {
 #    fi
     cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_TESTING=ON -DCODE_COVERAGE=ON -S "${SRC}" -B "${BUILD_FOLDER}" -DCMAKE_MODULE_LINKER_FLAGS_INIT=-L"${llvm_path_root}"lib/ -DCMAKE_SHARED_LINKER_FLAGS_INIT=-L"${llvm_path_root}"lib/ -DCMAKE_EXE_LINKER_FLAGS_INIT=-L"${llvm_path_root}"lib/ -DCMAKE_EXE_LINKER_FLAGS=-L"${llvm_path_root}"lib/ -DCMAKE_MODULE_LINKER_FLAGS=-L"${llvm_path_root}"lib/ -DCMAKE_SHARED_LINKER_FLAGS=-L"${llvm_path_root}"lib/
     cmake --build . -j "$((2 * $(getconf _NPROCESSORS_ONLN)))"
-    sudo supervisorctl reread
-    sudo supervisorctl update
-    sudo supervisorctl restart foxy_server
-    sudo supervisorctl status
 }
 
 get_last_modified_file() {
