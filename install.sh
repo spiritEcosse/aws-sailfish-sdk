132d34e313788fd76c7dccc878223223c30cda8b
spiritEcosse <shevchenkcoigor@gmail.com>
2024-05-04 00:15:47 +0200

    Update install.sh with additional package 'curl'.

--- install.sh
+++ install.sh
@@ -598,7 +598,7 @@ supervisorctl() {
 
 foxy_sever_libs() {
     system_prepare_ubuntu
-    install_for_ubuntu uuid-dev libjsoncpp-dev cmake make g++ g++-multilib zlib1g-dev supervisor jq libpq-dev micro unzip nlohmann-json3-dev libcurl4-openssl-dev libboost-all-dev git
+    install_for_ubuntu uuid-dev libjsoncpp-dev cmake make g++ g++-multilib zlib1g-dev supervisor jq libpq-dev micro unzip nlohmann-json3-dev libcurl4-openssl-dev libboost-all-dev git curl
     install_clang
 }
 
