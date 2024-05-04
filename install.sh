541434dde9c611d140b26dfee7227bdfa00f9c63
spiritEcosse <shevchenkcoigor@gmail.com>
2024-05-05 00:19:22 +0200

    Add function to set up instance server host to known hosts

--- install.sh
+++ install.sh
@@ -330,6 +330,7 @@ aws_start() {
 }
 
 rsync_from_host_to_sever() {
+    set_up_instance_server_host_to_known_hosts
     set_rsync_params
     rsync --rsync-path="sudo rsync" "${RSYNC_PARAMS_UPLOAD_SOURCE_CODE[@]}" --checksum --ignore-times --delete --include "3rdparty/*.cmake" --exclude "config.json" --exclude "3rdparty/*" --exclude "cmake-build-debug" "${BUILD_FOLDER}" "${SERVER_USER}@${SERVER_HOST}:~/${BUILD_FOLDER}" # TODO: check why i nee --checksum --ignore-times to transfer files to ec2 but it doesn't work for github action ubuntu ???
 }
