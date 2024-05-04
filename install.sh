d44d02374aa45b5618eef73858e39529025e5583
spiritEcosse <shevchenkcoigor@gmail.com>
2024-05-04 22:25:11 +0200

    Remove unnecessary wait command in install_clang function

--- install.sh
+++ install.sh
@@ -533,7 +533,6 @@ install_clang() {
         HTTP_FILE="https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_TAG}/${CLANG_FILE_FILE}"
         curl -O -L "${HTTP_FILE}"
 #        download_backup "${BACKUP_FILE_PATH}" "${HTTP_FILE}" "$(file_get_size "${HTTP_FILE}")" "$(python3 -c "print(100 * 1024 * 1024)")"
-        wait
         tar xf "${CLANG_FILE_FILE}"
 #        cd llvm-project
 #        mkdir -p build
