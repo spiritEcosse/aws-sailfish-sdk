f3eeef14c094bda67abf2d613ef44d5760e51528
spiritEcosse <shevchenkcoigor@gmail.com>
2024-06-03 20:52:43 +0200

    Remove unnecessary package 'gcc-multilib' for foxy_sever_libs installation

--- install.sh
+++ install.sh
@@ -615,7 +615,7 @@ supervisorctl() {
 
 foxy_sever_libs() {
     system_prepare_ubuntu
-    install_for_ubuntu uuid-dev libjsoncpp-dev cmake make g++ gcc-multilib zlib1g-dev supervisor jq libpq-dev micro unzip nlohmann-json3-dev libcurl4-openssl-dev libboost-all-dev git curl xz-utils rsync sudo sshpass
+    install_for_ubuntu uuid-dev libjsoncpp-dev cmake make g++ zlib1g-dev supervisor jq libpq-dev micro unzip nlohmann-json3-dev libcurl4-openssl-dev libboost-all-dev git curl xz-utils rsync sudo sshpass
     install_clang
     ls -lah
 }
